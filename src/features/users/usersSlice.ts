12e62303dc6d371e4f91e466c5e143ee02b62e45
Daniel Tonon <dan503.npm@gmail.com>
2024-07-03 13:12:06 +1000

    Applied slice based selectors to usersSlice
    This is optional, I think the standard way to create selectors is proabbly better as a standard

--- src/features/users/usersSlice.ts
+++ src/features/users/usersSlice.ts
@@ -17,13 +17,17 @@ const usersSlice = createSlice({
   name: 'users',
   initialState,
   reducers: {},
+  // Alternate method for writing selector functions
+  // WARNING! These only have access to the slice state, not the full root state!
+  selectors: {
+    selectAllUsers: (state) => state,
+    selectUserById: (state, userId: string | undefined) => state.find((u) => u.id === userId),
+  },
 })
 
 export const usersReducer = usersSlice.reducer
 
 // == SELECTORS ==
-export const selectAllUsers = (state: AppRootState) => state.users
-export const selectUserById = (state: AppRootState, userId: string | undefined) => {
-  return state.users.find((u) => u.id === userId)
-}
+export const { selectAllUsers, selectUserById } = usersSlice.selectors
+// This selector requires the use of the root state, so it cannot be written as a slice selector like the others
 export const selectCurrentUser = (state: AppRootState) => state.users.find((u) => u.id === selectCurrentUserId(state))
